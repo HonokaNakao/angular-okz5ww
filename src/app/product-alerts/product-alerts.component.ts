2bb93c96a69201221ce569286fcd215da5b04189
HonokaNakao <ho.sky.high@gmail.com>
2022-08-17 17:33:58 +0900

    1 file modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products' ;
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+  ],
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+
+  
+  }
+
+
+}
